cdd263a76a6f1502625df3de11299c03343bdea5
Moumy Kabir <mimokabir85@gmail.com>
2024-12-17 23:42:56 +0000

    Add Streamlit dashboard code and fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
+from Project.login import login_page
+from Project.dashboard import dashboard_page
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Initialize session state
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+if "page" not in st.session_state:
+    st.session_state.page = "login"  # Default page is "login"
+
+# Rerun logic for navigation
+if st.session_state.page == "dashboard" and st.session_state.logged_in:
+    dashboard_page()
+elif st.session_state.page == "login":
+    login_page()
